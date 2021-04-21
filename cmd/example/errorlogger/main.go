ef965b502022128e66c716659a1ec709ef12fc38
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-04-21 10:47:11 -0500

    add cmd/example/errorlogger/main.go (main Go run file).

---
+++ cmd/example/errorlogger/main.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Michael Treanor
+// https://github.com/skeptycal
+// MIT License
+
+// Example for package errorlogger.
+package main
+
+import "github.com/skeptycal/errorlogger"
+
+func main() {
+fmt.Println("Example for the errorlogger package.")
+errorlogger.Example()
+
+// add more code here ...
+
+}
