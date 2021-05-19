84764dbf62d7db5c98c6db02ebf5ed553a3f68f0
skeptycal <26148512+skeptycal@users.noreply.github.com>
2021-05-19 14:19:52 -0500

    remove boolean test values

--- error_func.go
+++ error_func.go
@@ -9,28 +9,27 @@ import "github.com/pkg/errors"
 // Disable disables logging and sets a no-op function for
 // Err() to prevent slowdowns while logging is disabled.
 func (e *errorLogger) Disable() {
-	e.enabled = false
 	e.errFunc = e.noErr
 }
 
 // Enable enables logging and restores the Err() logging functionality.
 func (e *errorLogger) Enable() {
-	e.enabled = true
 	e.errFunc = e.yesErr
 }
 
 // Err logs an error to the provided logger, if it is enabled,
-// and returns the error unchanged.
+// and returns the error unchanged to be propagated up.
 func (e *errorLogger) Err(err error) error {
 	return e.errFunc(err)
 }
 
-// noErr is a no-op placeholder for Err
+// noErr is a no-op errorFunc for disabling logging without
+// constant repetitive flag checks or other hacks.
 func (e *errorLogger) noErr(err error) error {
 	return err
 }
 
-// Err logs errors and passes them through unchanged.
+// yesErr is an errorFunc that logs and wraps an error, then returns the errorunchanged.
 func (e *errorLogger) yesErr(err error) error {
 	if err != nil {
 		if e.wrap != nil {
