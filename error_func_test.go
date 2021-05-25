31f5431f2595738514eb89f158d20b2e98c4fedd
skeptycal <26148512+skeptycal@users.noreply.github.com>
2021-05-25 14:10:44 -0500

    add nopWriter for testing; move test config to test_info.go

--- error_func_test.go
+++ error_func_test.go
@@ -9,19 +9,56 @@ import (
 )
 
 var (
-	e = newTestStruct(true, nil, nil, nil)
-	w = newTestStruct(true, fakeSysCallError, nil, nil)
-
-	errFuncList = []struct {
-		name string
-		fn   func(err error) error
-	}{
-		{"noErr", e.noErr},
-		{"yesErr", e.yesErr},
-		{"noErr", w.noErr},
-		{"yesErr", w.yesErr},
-	}
+	fakeOuter   error
+	yesnologger = New()
 )
+
+type nopWriter struct{}
+
+func (nopWriter) Write(b []byte) (n int, err error) {
+	return 0, nil
+}
+
+func init() {
+	yesnologger.SetOutput(nopWriter{})
+}
+
+// noErr is a no-op errorFunc for disabling logging without
+// constant repetitive flag checks or other hacks.
+// https://en.wikipedia.org/wiki/NOP_(code)
+func (e *errorLogger) noErr1(err error) error {
+	// TODO does this really need to return an error?
+	// TODO does the compiler remove this?
+	// TODO would a pointer be better here? *(&err)
+	// TODO does the compiler remove this?
+	return *(&err)
+}
+
+func Benchmark_errorLogger_noErr_yesErr(b *testing.B) {
+	tests := []struct {
+		name  string
+		input error
+		fn    LoggerFunc
+		want  error
+	}{
+		// TODO: Add test cases.
+		{"fake error", errFake, errFake}, // return an error unchanged
+		{"nil", nil, nil},                // return nil unchanged
+	}
+
+	for _, bb := range tests {
+		for _, test := range errFuncTestList {
+			b.Run(test.name+"("+bb.name+")", func(b *testing.B) {
+				var fake error
+				for i := 0; i < b.N; i++ {
+					fake = test.fn(bb.input)
+
+				}
+				fakeOuter = fake
+			})
+		}
+	}
+}
 
 func Test_errorLogger_noErr_yesErr(t *testing.T) {
 	tests := []struct {
@@ -30,12 +67,12 @@ func Test_errorLogger_noErr_yesErr(t *testing.T) {
 		want  error
 	}{
 		// TODO: Add test cases.
-		{"fake error", fakeError, fakeError}, // return an error unchanged
-		{"nil", nil, nil},                    // return nil unchanged
+		{"fake error", errFake, errFake}, // return an error unchanged
+		{"nil", nil, nil},                // return nil unchanged
 	}
 
 	for _, tt := range tests {
-		for _, test := range errFuncList {
+		for _, test := range errFuncTestList {
 			t.Run(test.name+"("+tt.name+")", func(t *testing.T) {
 				if got := test.fn(tt.input); got != tt.want {
 					t.Errorf("errorLogger.noErr() error = got %v, want %v", got, tt.want)
