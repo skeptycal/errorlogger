d7fe6c7c5ba021d7a854a8a1bc7bcd8aca568664
skeptycal <26148512+skeptycal@users.noreply.github.com>
2021-05-25 14:07:10 -0500

    move test config to test_info.go

--- errorlogger_test.go
+++ errorlogger_test.go
@@ -6,82 +6,15 @@ package errorlogger
 
 import (
 	"errors"
-	"os"
 	"testing"
-
-	"github.com/sirupsen/logrus"
 )
-
-var (
-	testDefaultLogger ErrorLogger    = New()
-	testLogrusLogger  *logrus.Logger = logrus.New()
-	fakeError         error          = errors.New("fake")
-	fakeSysCallError  error          = os.NewSyscallError("fake syscall error", errors.New("fake syscall error"))
-)
-
-func newTestStruct(enabled bool, wrap error, fn func(args ...interface{}), logger *logrus.Logger) *errorLogger {
-
-	if fn == nil {
-		fn = DefaultLogFunc
-	}
-
-	if wrap == nil {
-		// the defaultErrWrap is actually nil ... so this is not needed.
-		// However, if the default is later changed to a package-wide
-		// wrapper, this will be a valid check
-		wrap = DefaultErrWrap
-	}
-
-	if logger == nil {
-		logger = defaultlogger
-	}
-
-	e := &errorLogger{
-		wrap:    DefaultErrWrap,
-		logFunc: DefaultLogFunc,
-		Logger:  defaultlogger,
-	}
-	if enabled {
-		e.Enable()
-		return e
-	}
-	e.Disable()
-	return e
-}
 
 func TestNew(t *testing.T) {
-	tests := []struct {
-		name    string
-		input   interface{}
-		wantErr bool
-	}{
-		// control
-		{"global ErrorLogger", testDefaultLogger, false},
-		{"nil", nil, true},
-
-		// Test New() (should pass) and nil (should fail)
-		{"New()", New(), false},
-
-		// Also tests NewWithOptions()
-		{"NewWithOptions(false, nil, nil) (should pass)", NewWithOptions(false, nil, nil), false},
-		{"logrus logger alone", logrus.Logger{}, true},
-
-		{"NewWithOptions(true, nil, nil)", NewWithOptions(true, nil, nil), false},
-		{"NewWithOptions(true, DefaultLogFunc, DefaultErrWrap)", NewWithOptions(true, DefaultLogFunc, DefaultErrWrap), false},
-		{"NewWithOptions(false, DefaultLogFunc, nil)", NewWithOptions(true, DefaultLogFunc, nil), false},
-
-		// Various tests using private struct
-		{"logrus logger in errorLogger (not public)", &errorLogger{Logger: &logrus.Logger{}}, false},
-		{"default ErrorLogger with nil wrapper (not public)", &errorLogger{wrap: nil}, false},
-		// Do not need a check for this in the constructor since errorLogger is not exported
-		// But something to be aware of ...
-		// {"ErrorLogger with nil logger (should fail)", &errorLogger{Logger: nil}, true},
-	}
 
 	wantStruct := &errorLogger{Logger: defaultlogger}
 	var want ErrorLogger = wantStruct
 
-	for _, tt := range tests {
+	for _, tt := range errorloggerTests {
 		t.Run(tt.name, func(t *testing.T) {
 
 			got, ok := tt.input.(ErrorLogger)
@@ -110,7 +43,7 @@ func Test_errorLogger_SetErrorWrap(t *testing.T) {
 		input error
 		wrap  error
 	}{
-		{"fakeError", fakeError, fakeSysCallError},
+		{"fakeError", errFake, fakeSysCallError},
 		{"nil", nil, nil},
 	}
 	for _, tt := range tests {
