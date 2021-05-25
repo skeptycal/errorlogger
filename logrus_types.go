d00f4f21acb204629e7a72f0d6e11439a6653692
skeptycal <26148512+skeptycal@users.noreply.github.com>
2021-05-25 14:07:56 -0500

    add basicErrorLogger type for interface checks

--- logrus_types.go
+++ logrus_types.go
@@ -74,12 +74,48 @@ type (
 	// logrusLogger implements the most common functionality
 	// of the logging interface of the Logrus package.
 	//
+	// This is the minimum interface required for incorporation
+	// in an ErrorLogger.
+	//
 	// It is a compatible superset of the standard library
 	// log package and a compatible subset of the ErrorLogger
 	// package.
 	logrusLogger interface {
-		logrus.FieldLogger
+		basicErrorLogger
 		logrusCommonOptions
+	}
+
+	basicErrorLogger interface {
+		WithField(key string, value interface{}) *logrus.Entry
+		WithFields(fields logrus.Fields) *logrus.Entry
+		WithError(err error) *logrus.Entry
+
+		Debugf(format string, args ...interface{})
+		Infof(format string, args ...interface{})
+		Printf(format string, args ...interface{})
+		Warnf(format string, args ...interface{})
+		Warningf(format string, args ...interface{})
+		Errorf(format string, args ...interface{})
+		Fatalf(format string, args ...interface{})
+		Panicf(format string, args ...interface{})
+
+		Debug(args ...interface{})
+		Info(args ...interface{})
+		Print(args ...interface{})
+		Warn(args ...interface{})
+		Warning(args ...interface{})
+		Error(args ...interface{})
+		Fatal(args ...interface{})
+		Panic(args ...interface{})
+
+		Debugln(args ...interface{})
+		Infoln(args ...interface{})
+		Println(args ...interface{})
+		Warnln(args ...interface{})
+		Warningln(args ...interface{})
+		Errorln(args ...interface{})
+		Fatalln(args ...interface{})
+		Panicln(args ...interface{})
 	}
 
 	// logrusCommonOptions implements several common options
